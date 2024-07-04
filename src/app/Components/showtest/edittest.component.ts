97ab90b7f5733bf2aa481a242c8a66112e988f05
Madhuri <madhuri.bansode@algorisys.com>
2024-07-04 19:16:30 +0530

    Added random question feature on the 'Edit Test' page

--- src/app/Components/showtest/edittest.component.ts
+++ src/app/Components/showtest/edittest.component.ts
@@ -1,22 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { Response } from '@angular/http';
-import { Validators, FormBuilder } from '@angular/forms';
-import { GroupService } from '../../Services/group.service';
-import { CategoryService } from '../../Services/category.service';
-import { QuestionService } from '../../Services/question.service';
-import { ActivatedRoute } from '@angular/router';
-import { TestsService } from '../../Services/tests.service';
-import 'rxjs/add/operator/filter';
-import { Location } from '@angular/common';
-import { LoaderService } from '../../Services/loader.service';
+import { Component, OnInit } from "@angular/core";
+import { Response } from "@angular/http";
+import { Validators, FormBuilder } from "@angular/forms";
+import { GroupService } from "../../Services/group.service";
+import { CategoryService } from "../../Services/category.service";
+import { QuestionService } from "../../Services/question.service";
+import { ActivatedRoute } from "@angular/router";
+import { TestsService } from "../../Services/tests.service";
+import "rxjs/add/operator/filter";
+import { Location } from "@angular/common";
+import { LoaderService } from "../../Services/loader.service";
 @Component({
-  selector: 'app-edittest',
-  templateUrl: './edittest.component.html'
-
+  selector: "app-edittest",
+  templateUrl: "./edittest.component.html",
 })
 export class EdittestComponent implements OnInit {
-
-  message = '';
+  message = "";
   grpList: any;
   catList: any;
   questions: any;
@@ -28,25 +26,24 @@ export class EdittestComponent implements OnInit {
   testid: any;
   editData: any;
   isAutoCalcChked: boolean = false;
-    
+
   tblbtns: any[] = [
     {
-        title: 'Edit Test',
-        keys: ["Id"],
-        action: 'edit',
-        ishide: false,
-        isChkBox:true,
-        class:'class="fa fa-fw fa-check'
-    }
+      title: "Edit Test",
+      keys: ["Id"],
+      action: "edit",
+      ishide: false,
+      isChkBox: true,
+      class: 'class="fa fa-fw fa-check',
+    },
+  ];
 
-];
-
-columns: any[] = [
-  {label:'Id',name: '_id'}, 
-  {label:'Questions',name: 'text'}];
+  columns: any[] = [
+    { label: "Id", name: "_id" },
+    { label: "Questions", name: "text" },
+  ];
 
   constructor(
-
     private fb: FormBuilder,
     private groupService: GroupService,
     private categoryService: CategoryService,
@@ -54,48 +51,64 @@ columns: any[] = [
     private testsService: TestsService,
     private _Activatedroute: ActivatedRoute,
     private _location: Location,
-    private loaderService :LoaderService
+    private loaderService: LoaderService
   ) {
-    this.getQuestionList('All');
+    this.getQuestionList("All");
   }
-
 
   ngOnInit() {
     this.isAutoCalcChked = false;
     this.getGroupList();
     this.getCategoryList();
-    this._Activatedroute.params.subscribe(params => {
-      this.testid = params['_id'];
+    this._Activatedroute.params.subscribe((params) => {
+      this.testid = params["_id"];
     });
     this.testForm.controls["id"].setValue(this.testid);
     this.getTestsByID(this.testid);
-
   }
 
-
   testForm = this.fb.group({
-    id: [{ value: '', disabled: false }, (Validators.nullValidator)],
-    iscalculated: [{ value: '', disabled: false }, (Validators.nullValidator)],
-    qIds: [{ value: [], disabled: false }, (Validators.nullValidator)],
-    category: [{ value: [], disabled: false }, (Validators.required)],
-    testName: [{ value: '', disabled: false }, (Validators.required)],
-    testTime: [{ value: '', disabled: false }, (Validators.required)],
-    isActive: [{ value: '', disabled: false }, (Validators.nullValidator)],
-    isAutoCalc: [{ value: false, disabled: false }, (Validators.nullValidator)],
-    isShowResult: [{ value: false, disabled: false }, (Validators.nullValidator)],
-    groupid: [{ value: [], disabled: false }, (Validators.required)],
-
+    id: [{ value: "", disabled: false }, Validators.nullValidator],
+    iscalculated: [{ value: "", disabled: false }, Validators.nullValidator],
+    qIds: [{ value: [], disabled: false }, Validators.nullValidator],
+    category: [{ value: [], disabled: false }, Validators.required],
+    testName: [{ value: "", disabled: false }, Validators.required],
+    testTime: [{ value: "", disabled: false }, Validators.required],
+    isActive: [{ value: "", disabled: false }, Validators.nullValidator],
+    isAutoCalc: [{ value: false, disabled: false }, Validators.nullValidator],
+    isShowResult: [{ value: false, disabled: false }, Validators.nullValidator],
+    groupid: [{ value: [], disabled: false }, Validators.required],
+    numQuestions: ["", [Validators.required, Validators.min(1)]],
   });
 
-  get f() { return this.testForm.controls; }
+  get f() {
+    return this.testForm.controls;
+  }
 
   onSubmit() {
+    // if (this.selectedQuestions.length == 0) {
+    //   this.message = "Please select questions!";
+    //   this.submitted = false;
+    //   return;
+    // }
 
-    if (this.selectedQuestions.length == 0) {
-      this.message = "Please select questions!";
-      this.submitted = false;
-      return;
+    if (this.selectedQuestions.length === 0) {
+      const numQuestions = this.testForm.controls["numQuestions"].value;
+      this.selectedQuestions = this.getRandomQuestions(numQuestions);
+
+      this.testForm.controls["qIds"].setValue(this.selectedQuestions);
     }
+
+    // if (
+    //   this.selectedQuestions.length !==
+    //   this.testForm.controls["numQuestions"].value
+    // ) {
+    //   const numQuestions = this.testForm.controls["numQuestions"].value;
+    //   this.selectedQuestions = this.getRandomQuestions(numQuestions);
+
+    //   this.testForm.controls["qIds"].setValue(this.selectedQuestions);
+    // }
+
     this.testForm.controls["qIds"].setValue(this.selectedQuestions);
     this.testForm.controls["isActive"].setValue(true);
     this.testForm.controls["iscalculated"].setValue(false);
@@ -109,67 +122,74 @@ columns: any[] = [
     this.edit();
   }
 
-
-
   private edit() {
-      this.loaderService.appLoader =true;
-    this.testsService.updateTest(this.testForm.getRawValue())
-      .subscribe((response: Response) => {
+    this.loaderService.appLoader = true;
+    this.testsService.updateTest(this.testForm.getRawValue()).subscribe(
+      (response: Response) => {
         this.message = "Test updated successfully";
         this.submitted = false;
         this.testForm.reset();
         this.gotoParent();
-        this.getQuestionList('All');
+        this.getQuestionList("All");
         this.loaderService.appLoader = false;
-      }, error => {
-      });
+      },
+      (error) => {}
+    );
   }
 
   populateTestForm(data) {
-
-
-    this.testForm.controls["qIds"].setValue(data[0]['qIds']);
-    this.testForm.controls["category"].setValue(data[0]['category']);
+    this.testForm.controls["qIds"].setValue(data[0]["qIds"]);
+    this.testForm.controls["category"].setValue(data[0]["category"]);
     this.getQuestionList(this.testForm.controls["category"].value);
-    this.testForm.controls["testTime"].setValue(data[0]['testTime'] / 60);
-    this.testForm.controls["testName"].setValue(data[0]['testName']);
-    this.testForm.controls["groupid"].setValue(data[0]['groupid']);
-    this.testForm.controls["isAutoCalc"].setValue(data[0]['isAutoCalc']);
-    this.testForm.controls["isShowResult"].setValue(data[0]['isShowResult']);
+    this.testForm.controls["testTime"].setValue(data[0]["testTime"] / 60);
+    this.testForm.controls["testName"].setValue(data[0]["testName"]);
+    this.testForm.controls["groupid"].setValue(data[0]["groupid"]);
+    this.testForm.controls["isAutoCalc"].setValue(data[0]["isAutoCalc"]);
+    this.testForm.controls["isShowResult"].setValue(data[0]["isShowResult"]);
+    this.testForm.controls["numQuestions"].setValue(data[0]["qIds"].length);
     for (const key of Object.keys(this.testForm.controls["qIds"].value)) {
       this.selectedQuestions.push(this.testForm.controls["qIds"].value[key]);
     }
   }
 
   getGroupList() {
-    this.loaderService.appLoader =true;
+    this.loaderService.appLoader = true;
     this.groupService.groupListService().subscribe((response: Response) => {
       this.grpList = response;
       this.loaderService.appLoader = false;
     });
   }
   getCategoryList() {
-    this.loaderService.appLoader =true;
-    this.categoryService.categoryListService().subscribe((response: Response) => {
-      this.catList = response;
-      this.loaderService.appLoader = false;
-    });
+    this.loaderService.appLoader = true;
+    this.categoryService
+      .categoryListService()
+      .subscribe((response: Response) => {
+        this.catList = response;
+        this.loaderService.appLoader = false;
+      });
   }
 
   getQuestionList(categoryId: string) {
-    this.loaderService.appLoader =true;
-    this.questionService.questionsByCategory(categoryId).subscribe((response: Response) => {
-      this.questions = response;
-      this.origQuestions = response;
-      this.loaderService.appLoader = false;
-    });
+    this.loaderService.appLoader = true;
+    this.questionService
+      .questionsByCategory(categoryId)
+      .subscribe((response: Response) => {
+        this.questions = response;
+        this.origQuestions = response;
+        this.loaderService.appLoader = false;
+      });
   }
- 
+
+  // Method to select random questions from available questions
+  private getRandomQuestions(num: number): any[] {
+    const shuffled = this.questions.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, num).map((q) => q._id);
+  }
+
   onSelect(obj) {
     if (obj[0].event.target.checked) {
-      this.selectedQuestions.push(obj[0].option._id)
-    }
-    else if (!obj[0].event.target.checked) {
+      this.selectedQuestions.push(obj[0].option._id);
+    } else if (!obj[0].event.target.checked) {
       let newSelectedQuestions = [];
       for (const key of Object.keys(this.selectedQuestions)) {
         if (this.selectedQuestions[key] != obj[0].option._id) {
@@ -183,9 +203,6 @@ columns: any[] = [
     }
   }
 
-  
-
-
   selecteCategory(values) {
     this.getQuestionList(this.testForm.controls["category"].value);
   }
@@ -194,14 +211,10 @@ columns: any[] = [
     this.testsService.getTestById(testid).subscribe((response: Response) => {
       this.editData = response;
       this.populateTestForm(this.editData);
-
     });
   }
-
 
   gotoParent() {
     this._location.back();
   }
-
-
 }
